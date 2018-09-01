2f9f7689dbcf2c36f23b762f127b19034d09f4af
archiewald <artur.kozubek1@gmail.com>
2018-09-01 13:16:51 +0200

    init kontra.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,28 @@
-import { HELLO } from './hello'
+import './utils/kontra'
+kontra.init();
 
-document.write(HELLO)
+let sprite = kontra.sprite({
+    x: 100,        // starting x,y position of the sprite
+    y: 80,
+    color: 'red',  // fill color of the sprite rectangle
+    width: 20,     // width and height of the sprite rectangle
+    height: 40,
+    dx: 2          // move the sprite 2px to the right every frame
+  });
+  
+  let loop = kontra.gameLoop({  // create the main game loop
+    update: function() {        // update the game state
+      sprite.update();
+  
+      // wrap the sprites position when it reaches
+      // the edge of the screen
+      if (sprite.x > kontra.canvas.width) {
+        sprite.x = -sprite.width;
+      }
+    },
+    render: function() {        // render the game state
+      sprite.render();
+    }
+  });
+  
+  loop.start();    // start the game
